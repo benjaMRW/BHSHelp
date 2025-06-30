2b87053de674c5963f23877a2636d94132a758d8
benjaMRW <164798713+benjaMRW@users.noreply.github.com>
2025-06-30 14:06:19 +1200

    Add files via upload

---
+++ BHSHelp/BhsHelp/app/routes.py
@@ -0,0 +1,42 @@
+from app import app
+from flask import render_template, abort
+from flask_sqlalchemy import SQLAlchemy  # no more boring old SQL for us!
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "school.db")
+db.init_app(app)
+      
+
+import app.models as models
+
+
+# basic route
+@app.route('/')
+def root():
+    return render_template('home.html', page_title='HomePage')
+
+# @app.route('/map')
+# def root():
+#     return render_template('map.html', page_title='BHSmap')
+
+# @app.route('/notices')
+# def root():
+#     return render_template('notices.html')
+
+# @app.route('/teacher_finder')
+# def root():
+#     return render_template('teacher_finder.html')
+
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+if __name__ == "__main__":
+   app.run(debug=True)
